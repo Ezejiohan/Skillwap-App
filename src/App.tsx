33d144873ffb248db94eb834da344c7373248987
Toyosi Taiwo <adekunletaiwo43@yahoo.com>
2026-03-16 14:15:07 +0100

    Create Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Navbar /> */}
+      <Navbar />
 
       <Routes>
         <Route path="/" element={<Home />} />
